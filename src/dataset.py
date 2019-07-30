24ad6857eaa92ac1c3ee195ac103be0ba8a29bf5
Yuchen Li <ychnlgy@gmail.com>
2019-07-30 12:19:58 -0400

    I'm an idiot

--- src/dataset.py
+++ src/dataset.py
@@ -12,7 +12,7 @@ class Dataset:
     """
 
     def __init__(self, spec, slice_size, step_size):
-        assert len(spec.shape) == 2
+        assert len(spec.shape) == 2, spec.shape
         self._spec = numpy.expand_dims(spec, 0)  # (1, freq, time)
         self._slice_size = slice_size
         self._step_size = step_size
